ea0b6dee6d7c940ac11908e60e925341f778f193
rladbwls77 <K51165480@gmail.com>
2025-04-09 12:48:54 +0900

    apple

---
+++ legacy/Lactur 01/err.c
@@ -0,0 +1,23 @@
+﻿int main()
+{
+	int i;
+	int a;
+	
+	i=3;
+	a=3;
+	
+
+	while(i)		
+	{
+		while(a){
+			
+			puts("오징어");
+			a=a-1;
+		}
+		puts("*맛있다");
+		i=i-1;
+		a=3;
+	}
+
+	return 0;
+}
---
+++ legacy/Lactur 01/main.c
@@ -0,0 +1,23 @@
+﻿t title_screen()
+{
+	puts("********************");
+	puts("  PHS's LOVEstory  *");
+	puts("********************");
+
+	puts("");
+	return 0;
+}
+
+int main()
+{
+	title_screen();
+	title_screen();
+	title_screen();
+	title_screen();
+	title_screen();
+
+	puts("dd");
+	return 0;
+
+
+
